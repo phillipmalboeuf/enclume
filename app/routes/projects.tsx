a9a612bc3a24dfad3f2e8abce87f7e3818fcc5fa
Phillip Malboeuf <phil@boeuf.coffee>
2019-03-06 11:26:38 -0500

    projects categories links

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,7 +1,8 @@
 
 import * as React from 'react'
-
 import { Link, RouteComponentProps, NavLink } from 'react-router-dom'
+import query from 'query-string'
+
 import { AppContext } from '../contexts/app'
 import { Icon } from '../components/icon'
 import { Picture } from '../components/picture'
@@ -24,23 +25,23 @@ export class Projects extends React.Component<Props, State> {
   }
 
   public render() {
+    let current_category = query.parse(this.props.location.search).category
     return <>
-      <main className='' role='main'>
+      <main className={`${current_category ? current_category === 'planning' ? 'blue_back' : 'light_green_back' : ''}`} role='main'>
         <div className='padded padded--big_top'>
           <nav className='grid grid--guttered'>
-            <div className='col'><NavLink className='header__link' to='/projects'>Tous</NavLink></div>
+            <div className='col'><Link className={`header__link${current_category ? '' : ' active'}`} to='/projects'>Tous</Link></div>
             {this.context.content.categories.map(category => <div className='col' key={category.fields.title}>
-              <NavLink className='header__link' to={`/projects?tags=${category.fields.key}`}><LE c={category} k='title' /></NavLink>
+              <Link className={`header__link${current_category === category.fields.key ? ' active' : ''}`} to={`/projects?category=${category.fields.key}`}><LE c={category} k='title' /></Link>
             </div>)}
           </nav>
 
           <div className='normal_bottom' />
 
           <div className='grid grid--guttered'>
-            {this.context.content.projects.map(project => <div key={project.fields.url} className='col col--4of12'>
+            {this.context.content.projects.filter(project => !current_category || project.fields.category.fields.key === current_category).map(project => <div key={project.fields.url} className='col col--4of12'>
               <Link to={`/projects/${project.fields.url}`}>
-                {console.log(project.fields)}
-                <div className=''><LPE c={project} k='hero' /></div>
+                <div className='small_bottom'><LPE c={project} k='hero' /></div>
                 <p className='slight'>
                   <LE c={project} k='title' />
                 </p>
